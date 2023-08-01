bffe9e899cefc90e23a53458f298ebb29273e323
alalade191 <alalde191@gmail.com>
2023-08-01 22:47:40 +0100

    second commit

--- components/double-carousel.tsx
+++ components/double-carousel.tsx
@@ -1,13 +1,53 @@
-import { PietileCarousel } from "pietile-carousel";
+import { Carousel } from "@mantine/carousel";
+import Image from "next/image";
 
 function DoubleRoom() {
   return (
-    <PietileCarousel autoplayInterval={3} margin={20}>
-      <figure className="bg-[url(/images/double1-carousel.jpg)] w-[300] h-[430px] bg-cover bg-no-repeat bg-center rounded-md"></figure>
-      <figure className="bg-[url(/images/double2-carousel.jpg)] w-[300]  h-[430px]  bg-cover bg-no-repeat bg-center rounded-md"></figure>
-      <figure className="bg-[url(/images/double3-carousel.jpg)] w-[300]  h-[430px]  bg-cover bg-no-repeat bg-center rounded-md"></figure>
-    </PietileCarousel>
+    <Carousel
+      maw={600}
+      mx="auto"
+      withIndicators
+      height={430}
+      dragFree
+      slideGap="md"
+      align="center"
+      style={{ height: "400px" }}
+    >
+      <Carousel.Slide>
+        <div className="h-[430px] w-[600px]">
+          <Image
+            height={500}
+            width={600}
+            src={"/images/double1-carousel.jpg"}
+            alt=""
+            className="h-full"
+          />
+        </div>
+      </Carousel.Slide>
+      <Carousel.Slide>
+        <div className="h-[430px] w-[600px]">
+          <Image
+            height={100}
+            width={600}
+            src={"/images/double2-carousel.jpg"}
+            alt=""
+            className="h-full rounded-md"
+          />
+        </div>
+      </Carousel.Slide>
+      <Carousel.Slide>
+        <div className="h-[430px] w-[600px]">
+          <Image
+            height={100}
+            width={600}
+            src={"/images/double3-carousel.jpg"}
+            alt=""
+            className="h-full rounded-md"
+          />
+        </div>
+      </Carousel.Slide>
+      {/* ...other slides */}
+    </Carousel>
   );
 }
-
 export default DoubleRoom;
